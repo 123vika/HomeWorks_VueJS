cfe4fe0eea9cfccbb22bbe2e4698d67cff18b8d6
Victoria <123vika@gmail.com>
2021-07-28 20:33:09 +0300

    creating branch

---
+++ src/components/Calculator.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="calc">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data: () => ({
+    message: 'Hello Vue!! '
+  }),
+}
+</script>
